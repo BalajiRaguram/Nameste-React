85b8cc9258c5b9749409a1e1a655f208ad8b34f4
balaji <balajir.13391@gmail.com>
2023-04-02 11:01:33 +0530

    react commit1

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const head = React.createElement("h1",{id:"header"},"this is rendered from react ");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const head = React.createElement(
+  "h1",
+  { id: "header" },
+  "this is rendered from react "
+);
+
+const Title = () => {
+  return (
+    <div>
+      <h1> This is from title tag componenet</h1>
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title></Title>
+      <h1>this is the head componenet 💃 </h1>
+    </div>
+  );
+};
+
+const jsxhead = <h1> this is react application 🚀 </h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
- 
-root.render(head);
+
+root.render(<HeaderComponent />);
